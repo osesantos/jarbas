65810294d583b82070179cef228d0ea4efcb1bfa
José Santos <jose.dos.santos@outsystems.com>
2025-05-08 16:08:48 +0100

    Adding gin

--- commands/server.go
+++ commands/server.go
@@ -12,9 +12,18 @@ func Serve() resulto.ResultAny {
 	fmt.Println("Server is running...")
 
 	router := gin.Default()
+
+	// /ping
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+
+	// /
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
 		})
 	})
 
