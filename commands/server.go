fc08cc73fc8d7f7a3eaf6d2cd1ba39d9cc1ba1a7
José Santos <jose.dos.santos@outsystems.com>
2025-05-08 11:32:01 +0100

    Adding gin

--- commands/server.go
+++ commands/server.go
@@ -2,12 +2,23 @@ package commands
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/osesantos/resulto"
 )
 
 func Serve() resulto.ResultAny {
 	fmt.Println("Server is running...")
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run()
 
 	return resulto.SuccessAny()
 }
